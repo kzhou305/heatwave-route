86b5ee52977bc7a990470e7e65b1e8a5d3fda142
kzhou305 <zkaiyu01@gmail.com>
2025-06-16 08:47:58 -0400

    Update plot_segment.py

--- backend/plot_segment.py
+++ backend/plot_segment.py
@@ -1,1 +1,14 @@
+import pandas as pd
+import matplotlib.pyplot as plt
 
+def plot_seg_index(seg_csv, nid_list):
+    df = pd.read_csv(seg_csv)
+    df = df[df['nid'].isin(nid_list)].sort_values('nid')
+    plt.plot(df['nid'], df['pct_tree'], label='Tree')
+    plt.plot(df['nid'], df['pct_building'], label='Building')
+    plt.plot(df['nid'], df['pct_road'], label='Road')
+    plt.xlabel('NID')
+    plt.ylabel('Percent')
+    plt.legend()
+    plt.tight_layout()
+    plt.savefig('../output/comfort_chart.png')

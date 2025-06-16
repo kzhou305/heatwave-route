5e4f2fde1b775b20a706f84fbcb64e034c8bf5cf
kzhou305 <zkaiyu01@gmail.com>
2025-06-16 08:47:34 -0400

    Update gsv_fetcher.py

--- backend/gsv_fetcher.py
+++ backend/gsv_fetcher.py
@@ -1,1 +1,16 @@
+import os
+from shapely.geometry import LineString, Point
 
+def get_gsv_images(path):
+    line = LineString(path['geometry']['coordinates'])
+    images = []
+    for root, _, files in os.walk("../data/gsv"):
+        for f in files:
+            if f.endswith(".jpg") and "Location_" in f:
+                try:
+                    coord = f.split("Location_")[1].split("-heading")[0]
+                    lon, lat = map(float, coord.split(","))
+                    if line.buffer(0.0002).contains(Point(lon, lat)):
+                        images.append(f)
+                except: continue
+    return sorted(images)
